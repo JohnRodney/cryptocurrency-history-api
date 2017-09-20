0a086950b123183c13ff8c8b6c4065fad5363678
John Rodney <johnrodney@Johns-MacBook-Pro.local>
2017-09-20 09:42:48 -0500

    coments for later

--- dist/src/popperazi.js
+++ dist/src/popperazi.js
@@ -17,17 +17,23 @@ var _getUrlFromSym = require('./utilities/getUrlFromSym');
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var count = 0;
+/* Catch the error and report the error count and symbol if the call to URL
+ * fails to screenshot */
 var err = function err(next, res) {
   return function (err) {
-    return res(console.log(next, ++count));
+    return res(console.log(err, next, ++count));
   };
 };
+/* Perform the screenshot generating a url and path from the symbol
+ *   webshot spins up phantomjs and takes an image of the chart then saves it
+ *   to the server's public directory */
 var snapshot = function snapshot(next) {
   return function (res, rej) {
     return (0, _webshot2.default)((0, _getUrlFromSym.getURL)(next), (0, _getUrlFromSym.getPath)(next), _webshotOptions2.default, err(next, res));
   };
 };
 
+/* Iterate over the array of symbols and take a screenshot and save an image for each one */
 _symbols2.default.reduce(function (acc, next) {
   return acc.then(function () {
     return new Promise(snapshot(next));
--- src/popperazi.js
+++ src/popperazi.js
@@ -4,9 +4,15 @@ import webshotOptions from './settings/webshot-options';
 import { getURL, getPath } from './utilities/getUrlFromSym';
 
 let count = 0;
-const err = (next, res) => ((err) => res(console.log(next, ++count)));
+/* Catch the error and report the error count and symbol if the call to URL
+ * fails to screenshot */
+const err = (next, res) => ((err) => res(console.log(err, next, ++count)));
+/* Perform the screenshot generating a url and path from the symbol
+ *   webshot spins up phantomjs and takes an image of the chart then saves it
+ *   to the server's public directory */
 const snapshot = (next) =>
   ((res, rej) => webshot(getURL(next), getPath(next), webshotOptions, err(next, res)));
 
+/* Iterate over the array of symbols and take a screenshot and save an image for each one */
 symbols.reduce((acc, next) => acc.then(() => new Promise(snapshot(next))), Promise.resolve())
   .catch(err(null));
